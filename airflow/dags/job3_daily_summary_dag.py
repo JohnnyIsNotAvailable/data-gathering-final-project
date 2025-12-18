9429cefedcb0f966375b62bd6ec6cc5873c1e3d9
DaniilShim <d_shim@kbtu.kz>
2025-12-18 22:01:45 +0500

    dag

--- airflow/dags/job3_daily_summary_dag.py
+++ airflow/dags/job3_daily_summary_dag.py
@@ -27,17 +27,3 @@ def run_daily_analytics(**context):
     run_analytics(target_date=execution_date)
 
 
-with DAG(
-    dag_id='job3_daily_summary',
-    default_args=default_args,
-    description='Daily analytics job: SQLite events -> aggregations -> summary table',
-    schedule='@daily',
-    start_date=datetime(2024, 1, 1),
-    catchup=False,
-    tags=['analytics', 'sqlite', 'daily', 'batch'],
-) as dag:
-
-    analytics_task = PythonOperator(
-        task_id='compute_daily_summary',
-        python_callable=run_daily_analytics,
-    )
